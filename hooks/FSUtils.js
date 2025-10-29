681ad3598c7db9036b0fb24a8025952503d87ebe
zahhar ismail <zahhar.ismail@gmail.com>
2025-10-29 17:50:24 +0300

    add prepare plugin fix

--- hooks/FSUtils.js
+++ hooks/FSUtils.js
@@ -16,28 +16,68 @@
 "use strict";
 
 var fs = require("fs");
+var path = require("path");
 
 var FSUtils = (function () {
-    var api = {};
+  var api = {};
 
-    api.exists = function (path) {
-        try {
-            return fs.existsSync(path);
-        } catch (err) {
-            /*NOPE*/
-        }
-        return false;
-    };
+  api.exists = function (path) {
+    try {
+      return fs.existsSync(path);
+    } catch (err) {
+      /*NOPE*/
+    }
+    return false;
+  };
 
-    api.readFile = function (path, encoding) {
-        return fs.readFileSync(path, encoding);
-    };
+  api.copyFile = function (src, dest) {
+    fs.copyFileSync(src, dest);
+  };
 
-    api.writeFile = function (path, content) {
-        fs.writeFileSync(path, content);
-    };
+  api.readFile = function (path, encoding) {
+    return fs.readFileSync(path, encoding);
+  };
 
-    return api;
+  api.writeFile = function (path, content) {
+    fs.writeFileSync(path, content);
+  };
+
+  api.moveFile = function (oldPath, newPath) {
+    fs.renameSync(oldPath, newPath);
+  };
+
+  api.getFilesPath = function (startPath, filter) {
+    if (!fs.existsSync(startPath)) {
+      console.log("no dir ", startPath);
+      return;
+    }
+
+    let IsFilterArray = Array.isArray(filter);
+
+    let arry = [];
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+      var filename = path.join(startPath, files[i]).replace(/^.*[\\\/]/, "");
+      if (
+        IsFilterArray
+          ? EndWithArray(filename, filter)
+          : filename.endsWith(filter)
+      ) {
+        arry.push(filename);
+      }
+    }
+    return arry;
+  };
+
+  return api;
 })();
+
+function EndWithArray(filename, filterArry) {
+  let value = false;
+  value = filterArry.some((element) => {
+    return filename.endsWith(element);
+  });
+  return value;
+}
 
 module.exports = FSUtils;
